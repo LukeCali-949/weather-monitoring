1f3f85c47dd89a5677fdf76bef2fe216038ab4d3
LukeCali-949 <mengluke17@gmail.com>
2024-02-12 09:22:37 -0500

    commit before class ends

--- src/main/java/edu/iu/habahram/weathermonitoring/model/StatisticsDisplay.java
+++ src/main/java/edu/iu/habahram/weathermonitoring/model/StatisticsDisplay.java
@@ -1,4 +1,15 @@
 package edu.iu.habahram.weathermonitoring.model;
 
 public class StatisticsDisplay {
+    public String weather = "Cloudy";
+    public int avgTemp;
+    public int maxTemp;
+    public int minTemp;
+
+    StatisticsDisplay(String weather, int avgTemp, int maxTemp, int minTemp){
+        this.weather = weather;
+        this.avgTemp = avgTemp;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+    }
 }
